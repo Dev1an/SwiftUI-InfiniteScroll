c0e087c86dd7fc25f3f4af85584e6ff266f7fcce
Damiaan Dufaux <damiaan@dpro.be>
2021-07-19 15:44:20 +0200

    Update Floating.swift

--- InfiniteScroll/Floating.swift
+++ InfiniteScroll/Floating.swift
@@ -7,59 +7,48 @@
 
 import SwiftUI
 
-extension View {
-	func float<Content: View>(above: Content) -> ModifiedContent<Self, Above<Content>> {
-		self.modifier(Above(aboveContent: above))
-	}
-	func float<Content: View>(below: Content) -> ModifiedContent<Self, Below<Content>> {
-		self.modifier(Below(belowContent: below))
-	}
-	func float<Content: View>(trailing: Content) -> ModifiedContent<Self, Trailing<Content>> {
-		self.modifier(Trailing(trailingContent: trailing))
-	}
-}
-
-struct Above<AboveContent: View>: ViewModifier {
-	let aboveContent: AboveContent
-
-	func body(content: Content) -> some View {
-		content.overlay(
-			aboveContent.alignmentGuide(.top) { $0[.bottom] },
-			alignment: .top
+extension View { // Horizontal floating
+	func float<Content: View>(_ content: Content, side: Opposable<HorizontalAlignment>) -> some View {
+		overlay(
+			content.alignmentGuide(side.content) { $0[side.opposite] },
+			alignment: .init(horizontal: side.content, vertical: .center)
 		)
 	}
 }
 
-struct Below<AboveContent: View>: ViewModifier {
-	let belowContent: AboveContent
-
-	func body(content: Content) -> some View {
-		content.overlay(
-			belowContent.alignmentGuide(.bottom) { $0[.top] },
-			alignment: .bottom
+extension View { // Vertical floating
+	func float<Content: View>(_ content: Content, alignment: Opposable<VerticalAlignment>) -> some View {
+		overlay(
+			content.alignmentGuide(alignment.content) { $0[alignment.opposite] },
+			alignment: .init(horizontal: .center, vertical: alignment.content)
 		)
 	}
 }
 
-struct Trailing<AboveContent: View>: ViewModifier {
-	let trailingContent: AboveContent
+struct Opposable<Content> {
+	let content: Content
+	let opposite: Content
+	init(_ value: Content, opposite oppositeValue: Content) { (content, opposite) = (value, oppositeValue) }
+}
 
-	func body(content: Content) -> some View {
-		content.overlay(
-			trailingContent.alignmentGuide(.trailing) { $0[.leading] },
-			alignment: .trailing
-		)
-	}
+extension Opposable where Content == VerticalAlignment {
+	static let top = Opposable(.top, opposite: .bottom)
+	static let bottom = Opposable(.bottom, opposite: .top)
+}
+
+extension Opposable where Content == HorizontalAlignment {
+	static let leading = Opposable(.leading, opposite: .trailing)
+	static let trailing = Opposable(.trailing, opposite: .leading)
 }
 
 struct Floating_Previews: PreviewProvider {
 	static var previews: some View {
 		HStack {
-			Text("Block 1").float(above: TextBox(content: "This floats above 1"))
+			Text("Block 1").float(TextBox(content: "This floats above 1"), alignment: .top)
 			Divider()
 			Text("Block 2")
 			Divider()
-			Text("Block 3").float(below: TextBox(content: "This floats below 3"))
+			Text("Block 3").float(TextBox(content: "This floats below 3"), alignment: .bottom)
 		}.fixedSize()
 	}
 
